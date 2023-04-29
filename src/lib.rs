6b658339b93705fc16894ce36156d933e7bdad3c
apoclyps <kyle90adam@hotmail.com>
2023-04-29 14:04:39 +0100

    adds conditional logic to render html

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub fn app() -> Html {
         favorite_language: "Rust".to_owned(),
     };
     let class: &str = "title";
+    let message: Option<&str> = Some("I am a message");
 
     log!("username is ", name);
     log!(serde_json::to_string_pretty(&my_object).unwrap());
@@ -23,7 +24,17 @@ pub fn app() -> Html {
     html! {
         <>
             <h1 class={class}>{ "Hello World!" }</h1>
-            <p>{"Hi there!"}</p>
+            if class == "title" {
+                <p>{"Hi there!"}</p>
+            } else {
+                <p>{"Not a title"}</p>
+            }
+
+            if let Some(message) = message {
+                <p>{message}</p>
+            } else {
+                <p>{"No message"}</p>
+            }
         </>
     }
 }
