7a5768716ee79938f1b871acf51e3f2e1b75c8e0
apoclyps <kyle90adam@hotmail.com>
2023-05-04 10:34:53 +0100

    programatic linking to pages

--- src/components/pages/about.rs
+++ src/components/pages/about.rs
@@ -1,8 +1,17 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::router::Route;
 
 #[function_component(About)]
 pub fn about() -> Html {
+    let navigator: Navigator = use_navigator().unwrap();
+    let onclick: Callback<MouseEvent> = Callback::from(move |_| navigator.push(&Route::Home));
+
     html! {
+        <>
         <p>{"About"}</p>
+        <button onclick={onclick}>{"Go home"}</button>
+        </>
     }
 }
--- src/components/pages/home.rs
+++ src/components/pages/home.rs
@@ -1,8 +1,13 @@
+use crate::router::Route;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
+        <>
         <p>{"Home"}</p>
+        <Link<Route> to={Route::About}>{"About"}</Link<Route>>
+        </>
     }
 }
